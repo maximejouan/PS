142d5e4c9a2b431b680da8662b155534d6a3d109
maximejouan <51750595+maximejouan@users.noreply.github.com>
2019-06-13 12:03:04 +0200

    Update AD.ps1

--- AD.ps1
+++ AD.ps1
@@ -1,4 +1,4 @@
 Install-windowsfeature AD-domain-services
 Import-Module ADDSDeployment
 
-Install-ADDSForest -DomainName max.local -CreateDNSDelegation -DomainMode Win2016 -ForestMode Win2016 -DomainNetbiosName "max" -SafeModeAdministratorPassword (ConvertTo-SecureString "Keepcool33!" -AsPlainText -Force) -InstallDNS:$true
+Install-ADDSForest -DomainName max.local -CreateDNSDelegation -DomainNetbiosName "max" -SafeModeAdministratorPassword (ConvertTo-SecureString "Keepcool33!" -AsPlainText -Force) -InstallDNS:$true
