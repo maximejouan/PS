ed0e6d5404ff433e09df370cf60e6cdcc96a2f70
maximejouan <51750595+maximejouan@users.noreply.github.com>
2019-06-13 14:36:46 +0200

    Update AD.ps1

--- AD.ps1
+++ AD.ps1
@@ -1,4 +1,2 @@
 Install-windowsfeature AD-domain-services
-Import-Module ADDSDeployment
-
 Install-ADDSForest -DomainName max.local -DomainNetbiosName "max" -SafeModeAdministratorPassword (ConvertTo-SecureString "Keepcool33!" -AsPlainText -Force) -InstallDNS:$true -confirm -force -NoRebootOnCompletion:$false
