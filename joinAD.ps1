9e9be45bae0c9fbd86557427ef89b28a2d4a70f9
maximejouan <51750595+maximejouan@users.noreply.github.com>
2019-06-14 09:44:19 +0200

    Update joinAD.ps1

--- joinAD.ps1
+++ joinAD.ps1
@@ -1,6 +1,6 @@
 echo "test" >> C:\TEMP\log.txt
 $domainnetbios = "max"
-$domainFQDFN = "max.local"
+$domainFQDN = "max.local"
 $password = "Keepcool33!" | ConvertTo-SecureString -asPlainText -Force
 $username = "$domainnetbios\admincis" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
